b0f044fe21432f92e7aa6b1162ab8c0137752f6f
Batstako <stanislav.svetoslavov@gmail.com>
2017-05-07 20:59:22 +0300

    Add Nunit Console and Nunit Runner

---
+++ Blog-Skeleton/Blog.UI.Tests/Host.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.Seleno;
+using TestStack.Seleno.Configuration;
+
+namespace Blog.UI.Tests
+{
+    public static class BrowserHost
+    {
+        public static readonly SelenoHost Instance = new SelenoHost();
+        public static readonly string RootUrl = "http://localhost:60634/Article/List";
+
+        static BrowserHost()
+        {
+            Instance.Run("Blog", 60634); // това пуска само срещу Firefox
+            //Instance.Run("Blog", 60634, configure => configure.WithRemoteWebDriver(BrowserFactory.Chrome)); // Това пуска на Chrome
+        }
+    }
+}
--- Blog-Skeleton/Blog.UI.Tests/UITests.cs
+++ Blog-Skeleton/Blog.UI.Tests/UITests.cs
@@ -1,20 +1,32 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
+using NUnit.Framework;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace Blog.UI.Tests
 {
     [TestFixture]
     public class UITests
     {
+        
         [Test]
-        public void TestMethod()
+        public void CheckSiteLoad()
         {
-            // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            IWebDriver driver = BrowserHost.Instance.Application.Browser;
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+
+            driver.Navigate().GoToUrl("http://localhost:60634/Article/List");
+            var logo = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[@class='navbar-brand']")));
+            //var Logo1 = wait.Until(w => w.FindElement(By.XPath(".//a[@class = 'navbar - brand']")));
+
+            Assert.AreEqual("SOFTUNI BLOG", logo.Text);
         }
     }
 }
